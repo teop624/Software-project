6955701da4a476d741d2cd4869d3e3b6ae540c01
teop624 <teo.platter@education.nsw.gov.au>
2025-08-24 07:18:35 +0000

    trying to make search button work

--- main.py
+++ main.py
@@ -56,37 +56,48 @@ class Main(object):
         centerRightFrame.pack(side = RIGHT)
 
         #search
-        search_bar = LabelFrame(centerRightFrame, width = 500, height = 75, text = 'search', bg = '#9bc9ff')
+        search_bar = LabelFrame(centerRightFrame, width = 440, height = 75, text = 'search', bg = '#9bc9ff')
         search_bar.pack(fill = BOTH)
+
         self.lbl_search = Label(search_bar, text = 'Search Book', font = 'arial 12 bold', bg = '#9bc9ff', fg = 'white')
         self.lbl_search.grid(row = 0, column = 0, padx = 20, pady = 10)
+
         self.ent_search = Entry(search_bar, width = 30, bd = 10)
-        self.ent_search.grid(row = 0, column = 1, columnspan = 3, padx = 10, pady = 10)
+        self.ent_search.grid(row = 0, column = 1, columnspan = 2, padx = 10, pady = 10)
+
         self.btn_search = Button(search_bar, text = 'Search', font = 'arial 12', bg = '#fcc324', fg = 'white', command = self.searchBooks)
-        self.btn_search.grid(row = 0, column = 4, padx = 20, pady = 10)
+        self.btn_search.grid(row = 0, column = 3, padx = 20, pady = 10)
+
         #list 
         list_bar = LabelFrame(centerRightFrame, width = 440, height = 175, text = 'list', bg = '#fcc324')
         list_bar.pack(fill = BOTH)
+
         lbl_list = Label(list_bar, text = 'Sort by', font = 'times 16 bold', fg = '#2488ff', bg = '#fcc324')
         lbl_list.grid(row = 0, column = 2)
+
         self.listChoice = IntVar()
         rb1 = Radiobutton(list_bar, text = 'All Books', var = self.listChoice, value = 1, bg = '#fcc324')
         rb2 = Radiobutton(list_bar, text = 'Acailable Books', var = self.listChoice, value = 2, bg = '#fcc324')
         rb3 = Radiobutton(list_bar, text = 'Borrowed Books', var = self.listChoice, value = 3, bg = '#fcc324')
+
         rb1.grid(row = 1, column = 0)
         rb2.grid(row = 1, column = 1)
         rb3.grid(row = 1, column = 2)
+
         btn_list = Button(list_bar, text = 'List Books', bg = '#2488ff', fg = 'white', font = 'arial 12')
         btn_list.grid(row = 1, column = 3, padx = 40, pady = 10)
 
         #title
         image_bar = Frame(centerRightFrame, width = 440, height = 350)
         image_bar.pack(fill = BOTH)
+
         self.title_right = Label(image_bar, text = 'Welcom to our Library', font = 'arial 16 bold')
         self.title_right.grid(row = 0)
+        
         self.img_library = PhotoImage(file = 'icons/library.png')
         self.lblImg = Label(image_bar, image = self.img_library)
         self.lblImg.grid(row = 1)
+
         #add book
         self.iconbook=PhotoImage(file='icons/addbook.png')
         self.btnbook = Button(topFrame, text = 'Add Book', image = self.iconbook, compound = LEFT, font = 'arial 12 bold', width = 100, height = 40, command = self.addBook)
