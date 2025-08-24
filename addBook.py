326f43331d50419845ec15b7a1a8b29cc5cf0791
teop624 <teo.platter@education.nsw.gov.au>
2025-08-24 05:39:20 +0000

    said Toplevel wasn't defined randomly so i had to explicitly import it, the new book & author buttons aren't working yet

--- addBook.py
+++ addBook.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import Toplevel
 from tkinter import messagebox
 import sqlite3
 
@@ -21,3 +22,16 @@ class AddBook(Toplevel):
         top_image_lbl.place(x = 120, y = 10)
         heading = Label(self.topFrame, text = 'Add Book', font = 'arial 22 bold', bg = 'white', fg = '#003f8a')
         heading.place(x = 290, y = 60)
+
+        #name
+        self.lbl_name = Label(self.bottomFrame, text = 'Book Name', font = 'arial 15 bold', bg = '#fcc324', fg = 'white')
+        self.lbl_name.place(x = 40, y = 40)
+        self.ent_name = Entry(self.bottomFrame, width = 30, bd = 4)
+        self.ent_name.insert(0, 'Enter Book Name')
+        self.ent_name.place(x = 150, y = 45)
+        #author
+        self.lbl_author = Label(self.bottomFrame, text = 'Author', font = 'arial 15 bold', bg = '#fcc324', fg = 'white')
+        self.lbl_author.place(x = 40, y = 80)
+        self.ent_author = Entry(self.bottomFrame, width = 30, bd = 4)
+        self.ent_author.insert(0, 'Enter Author Name')
+        self.ent_author.place(x = 150, y = 85)
