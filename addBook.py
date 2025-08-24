727177b2d811c7c2d5729f01a266e00326e736e8
teop624 <teo.platter@education.nsw.gov.au>
2025-08-24 05:18:45 +0000

    did more to add book window but still doesn't work, but maybe just the dev container

--- addBook.py
+++ addBook.py
@@ -8,3 +8,16 @@ class AddBook(Toplevel):
         self.geometry("650x750+550+200")
         self.title("Add Book")
         self.resizable(False,False)
+
+        #top frame
+        self.topFrame = Frame(self, height = 150, bg = 'white')
+        self.topFrame.pack(fill = X)
+        #bottom frame
+        self.bottomFrame = Frame(self, height = 600, bg = '#fcc324')
+        self.bottomFrame.pack(fill = X)
+        #heading, image & date
+        self.top_image = PhotoImage(file = 'icons/addbook.png')
+        top_image_lbl = Label(self.topFrame, image = self.top_image, bg = 'white')
+        top_image_lbl.place(x = 120, y = 10)
+        heading = Label(self.topFrame, text = 'Add Book', font = 'arial 22 bold', bg = 'white', fg = '#003f8a')
+        heading.place(x = 290, y = 60)
