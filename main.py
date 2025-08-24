afd70b2ef963bf062553d0de88711761fe387309
teop624 <teo.platter@education.nsw.gov.au>
2025-08-24 08:44:53 +0000

    error with window

--- main.py
+++ main.py
@@ -212,15 +212,35 @@ class GiveBook(Toplevel):
         self.resizable(False,False)
         global given_id
         self.book_id = int(given_id)
-        print(type(given_id))
+        query = "SELECT * FROM books"
+        books = cur.execute(query).fetchall()
+        book_list = []
+        for book in books:
+            book_list.append(str(book[0]) + '-' + book[1])
+
+        query2 = "SELECT * FROM members"
+        members = cur.execute(query2).fetchall()
+        member_list = []
+        for member in members:
+            member_list.append(str(member[0]) + '-' + member[1])
 
 
-        #top frame
-        self.topFrame = Frame(self, height = 150, bg = 'white')
-        self.topFrame.pack(fill = X)
-        #bottom frame
-        self.bottomFrame = Frame(self, height = 600, bg = '#fcc324')
-        self.bottomFrame.pack(fill = X)
+        #book name
+        self.book_name = StringVar()
+        self.lbl_name = Label(self.bottomFrame, text = 'Book Name', font = 'arial 15 bold', bg = '#fcc324', fg = 'white')
+        self.lbl_name.place(x = 40, y = 40)
+        self.combo_name = ttk.Combobox(self.bottomFrame, textvariable = self.book_name)
+        self.combo_name['values'] = book_list
+        self.combo_name.place(x = 150, y = 45)
+        #member name
+        self.member_name = StringVar()
+        self.lbl_member = Label(self.bottomFrame, text = 'Member Name', font = 'arial 15 bold', bg = '#fcc324', fg = 'white')
+        self.lbl_member.place(x = 40, y = 80)
+        self.combo_member = ttk.Combobox(self.bottomFrame, textvariable = self.member_name)
+        self.combo_member.place(x = 150, y = 85)
+        #button
+        button = Button(self.bottomFrame, text = 'Give Book', command = self.giveBook)
+        button.place(x = 150, y = 120)
         #heading, image & date
         self.top_image = PhotoImage(file = 'icons/givebook.png')
         top_image_lbl = Label(self.topFrame, image = self.top_image, bg = 'white')
